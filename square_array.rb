e921f8263a68c287f6f8441d8b560515c1f61bea
Nathan Rathe <nprathe10@gmail.com>
2019-05-29 22:00:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
   array.each do |new_numbers|
-    new_numbers = Math.sqrt(new_numbers)
+    new_numbers = new_numbers * new_numbers
     return "#{new_numbers}"
   end
 end
